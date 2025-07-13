9893c5be84d148fb3fb760fe1a03b48eb05a5116
Ari Croock <acroock@isazi.ai>
2025-07-13 16:49:30 +0200

    Initial Commit

---
+++ ProximalTests/ProximalTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProximalTests.swift
+//  ProximalTests
+//
+//  Created by Ari Croock on 2025/07/13.
+//
+
+import Testing
+@testable import Proximal
+
+struct ProximalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
